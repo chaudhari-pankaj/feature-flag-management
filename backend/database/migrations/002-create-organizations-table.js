fabdfe610c77a6ffea9aa1e93f0b2830fa5140b5
chaudhari-pankaj <chaudharigpankaj@gmail.com>
2026-07-01 00:35:45 +0530

    fix: made organization_name field unique

--- backend/database/migrations/002-create-organizations-table.js
+++ backend/database/migrations/002-create-organizations-table.js
@@ -3,7 +3,7 @@ import { pool } from '../connectDB.js'
 export const up = async() => {
     const createOrganizationsTableQuery = `CREATE TABLE IF NOT EXISTS organizations (
         organization_id VARCHAR(50) PRIMARY KEY,
-        organization_name VARCHAR(32) NOT NULL,
+        organization_name VARCHAR(32) NOT NULL UNIQUE,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )`;
 
