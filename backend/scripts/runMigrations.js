eb785cce37e8b5d2868e724660848c131339ae22
chaudhari-pankaj <chaudharigpankaj@gmail.com>
2026-06-30 23:31:48 +0530

    refactor: improved code structure

--- backend/scripts/runMigrations.js
+++ backend/scripts/runMigrations.js
@@ -5,15 +5,24 @@ import { randomUUID } from 'node:crypto';
 
 export const runMigrations = async() => {
     try {
+        //get all migration files
+        const files = await fs.readdir('../database/migrations');
+        files.sort();
+        
+        //create migrations table
+        let createMigrationsTable = await import(`../database/migrations/${files[0]}`);
         await createMigrationsTable.up();
+        console.log(`Migration file ${files[0]} successfully ran`);   
+
+        //get all the migration files that have been previously run
         const [response] = await pool.query('SELECT filename FROM migrations');
         let migratedFiles = [];
         for(let i = 0; i < response.length; i++) {
             migratedFiles.push(response[i].filename);
         }
-        const files = await fs.readdir('../database/migrations');
-        files.sort();
-        for(let i = 0; i < files.length; i++) {
+
+        //run migration files which have not been previously run
+        for(let i = 1; i < files.length; i++) {
             try {
                 let migrationFile = await import(`../database/migrations/${files[i]}`)
                 if(migratedFiles.includes(files[i]))
